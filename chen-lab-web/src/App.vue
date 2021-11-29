2236ab2891adc04c3c259bad9c635acf224d51ac
ParallelLight <zhangze21@mails.ucas.ac.cn>
2021-11-29 22:22:20 +0800

    element-plus

--- chen-lab-web/src/App.vue
+++ chen-lab-web/src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img src="./assets/logo-hias.png">
+  <div>
+    <p>
+      If Element Plus is successfully added to this project, you'll see an
+      <code v-text="'<el-button>'"></code>
+      below
+    </p>
+    <el-button type="primary">el-button</el-button>
+  </div>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Homepage msg="Welcome to ChenLab!"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Homepage from './components/Homepage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Homepage
   }
 }
 </script>
